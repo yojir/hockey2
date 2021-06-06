f96ffa2d01141cac11f4246f04c80fb0e2f54497
yojir <84670875+yojir@users.noreply.github.com>
2021-06-06 13:50:53 +0900

    change2
    ２回目の修正

--- index.js
+++ index.js
@@ -73,6 +73,7 @@ const mouseMoveHandler = (e) => {
 };
 
 const touchMoveHandler = (e) => {
+    e.preventDefault;
     let relativeXT = e.changedTouches[0].clientX - $canvas.offsetLeft;
     if(relativeXT > 0 && relativeXT < $canvas.width) {
         paddleXMW = relativeXT - paddleWidthE/2;
